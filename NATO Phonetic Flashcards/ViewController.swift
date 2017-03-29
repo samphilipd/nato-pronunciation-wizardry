928d04b400a94375f9b315d700b02fb9053c20b3
Sam Davies <seivadmas@gmail.com>
2017-03-29 11:51:39 +0100

    Initial Commit

---
+++ NATO Phonetic Flashcards/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NATO Phonetic Flashcards
+//
+//  Created by Sam Davies on 29/03/2017.
+//  Copyright © 2017 Sam Davies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
